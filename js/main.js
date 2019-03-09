e2edd1b5057c5e93b0fb76b05922c2041df04b30
Luísa Barros <qlullyq@gmail.com>
2019-03-08 21:03:50 -0300

    Add transitions and portfolio boxes links

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 (function(doc) {
-    let dropdownLink = doc.querySelector('#dropdownLink');
+    const dropdownLink = doc.querySelector('#dropdownLink');
+    // const toggleButton = doc.querySelector('[data-toggle="popover"]');
+
     dropdownLink.addEventListener('click', toggleDropDown, false);
+    // toggleButton.addEventListener('click', button, false);
 
     function toggleDropDown() {
         let navbarToggle = doc.querySelector('#navbar-toggle');
@@ -11,4 +14,9 @@
             navbarToggle.className = 'topnav';
         }
     }
+
+    // function button() {
+    //     toggleButton.popover();
+    // };
+
 })(document);
