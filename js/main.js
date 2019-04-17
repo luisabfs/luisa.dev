d742bbb3f5c4242da374afae22e11c2672a0b513
Luísa Barros <qlullyq@gmail.com>
2019-04-16 23:50:28 -0300

    Add language picker dropdown

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 (function(doc) {
     const dropdownLink = doc.querySelector('#dropdownLink');
-    // const toggleButton = doc.querySelector('[data-toggle="popover"]');
 
     dropdownLink.addEventListener('click', toggleDropDown, false);
-    // toggleButton.addEventListener('click', button, false);
 
     function toggleDropDown() {
         let navbarToggle = doc.querySelector('#navbar-toggle');
@@ -14,9 +12,4 @@
             navbarToggle.className = 'topnav';
         }
     }
-
-    // function button() {
-    //     toggleButton.popover();
-    // };
-
 })(document);
