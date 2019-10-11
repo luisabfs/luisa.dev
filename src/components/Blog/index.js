6e09f08083a6fc10dba6671f2ef2673584918320
Luísa Barros <qlullyq@gmail.com>
2019-10-10 22:41:38 -0300

    feat: create blog component

---
+++ src/components/Blog/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container, Wrapper, Card } from './styles';
+
+const Blog = () => (
+  <Container>
+    <h1>últimos posts no blog</h1>
+    <Wrapper>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </Wrapper>
+  </Container>
+);
+
+export default Blog;
---
+++ src/components/Blog/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 70%;
+  height: 80%;
+  margin: 40px 20px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+export const Card = styled.div`
+  width: 350px;
+  height: 260px;
+  background-color: #c4c4c4;
+  margin: 10px;
+`;
