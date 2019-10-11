11d1946eb702597656bf96f541c97f6994ad02c1
Luísa Barros <qlullyq@gmail.com>
2019-10-10 22:41:50 -0300

    feat: create instagram component

---
+++ src/components/Instagram/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Instagram = () => (
+  <Container />
+);
+
+export default Instagram;
---
+++ src/components/Instagram/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  width: 50%;
+  margin: 50px;
+  background-color: #c2c2c2;
+`;
