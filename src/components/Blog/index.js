c731ba55c8312a96a1ac96d78a3b87bfbd5f5eef
Luísa Barros <qlullyq@gmail.com>
2019-10-14 04:46:33 -0300

    feat: add blog details blur hover

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
 
-import { Container, Wrapper, Card } from './styles';
+import { Container, Wrapper, Card, CardDetails } from './styles';
+
+import thumbnail from '../../assets/images/thumbnail.png';
 
 const Blog = () => (
   <Container>
     <h1>últimos posts no blog</h1>
     <Wrapper>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card>
+        <img src={thumbnail} alt=""/>
+        <CardDetails>
+          <span>web</span>
+          <h1>VER MAIS</h1>
+          <span>2 dias</span>
+        </CardDetails>
+      </Card>
+      <Card>
+        <img src={thumbnail} alt=""/>
+        <CardDetails>
+          <span>web</span>
+          <h1>VER MAIS</h1>
+          <span>2 dias</span>
+        </CardDetails>
+      </Card>
+      <Card>
+        <img src={thumbnail} alt=""/>
+        <CardDetails>
+          <span>web</span>
+          <h1>VER MAIS</h1>
+          <span>2 dias</span>
+        </CardDetails>
+      </Card>
+      <Card>
+        <img src={thumbnail} alt=""/>
+        <CardDetails>
+          <span>web</span>
+          <h1>VER MAIS</h1>
+          <span>2 dias</span>
+        </CardDetails>
+      </Card>
     </Wrapper>
   </Container>
 );
--- src/components/Blog/styles.js
+++ src/components/Blog/styles.js
@@ -17,8 +17,45 @@ export const Wrapper = styled.div`
 `;
 
 export const Card = styled.div`
-  width: 300px;
-  height: 210px;
-  background-color: #c4c4c4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 350px;
+  height: 260px;
   margin: 10px;
+  border: 1px solid #e3e3e3;
+  border-radius: 4px;
+  /* background: url(${(props) => props.src}) no-repeat center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-attachment: scroll; */
+  transition: .2s ease-in-out;
+
+  &:hover {
+    img {
+      filter: blur(1px);
+      transition: .2s ease-in-out;
+    }
+
+    div {
+      transition: .3s ease-in-out;
+      visibility: visible;
+    }
+  }
+`;
+
+export const CardDetails = styled.div`
+  display: flex;
+  position: absolute;
+  flex-direction: column;
+  visibility: hidden;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  border: 1px solid #e3e3e3;
+  border-radius: 4px;
+  background-color: #fff;
+
 `;
