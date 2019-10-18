02c3d87541acb7efbf0b49e1ac4a57f2a763d6bf
Luísa Barros <qlullyq@gmail.com>
2019-10-18 11:46:49 -0300

    feat: add contact item

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -9,9 +9,8 @@ const Card = () => (
   <Container>
     <Avatar src={headshot} />
     <h1>Luísa Barros</h1>
-    <p>Eu tinha 11 anos quando entrei em contato pela primeira vez com
-     algoritmos. Desde então sou fascinada pela área de tecnologia,
-     especialmente programação. Agora, com 18 anos, estou determinada a
+    <p>Desde os 11 anos sou fascinada pela área de tecnologia,
+     especialmente programação. Agora, com 18, estou determinada a
      construir uma carreira na área de desenvolvimento de software.
     </p>
     <img src={layer} alt="" />
