ac80982c0ad4302251ba44b52cbde5f45f359a36
Luísa Barros <qlullyq@gmail.com>
2019-10-22 00:50:12 -0300

    feat: add main cards

--- src/pages/sobre/index.js
+++ src/pages/sobre/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import { IconContext } from 'react-icons';
 
 import {
-  FaGithub
+  FiTerminal,
+  FiAtSign,
+  FiStar,
+  FiCoffee,
+} from 'react-icons/fi';
+
+import {
+  FaJs,
+  FaNodeJs,
+  FaReact,
+  FaCode,
 } from 'react-icons/fa';
+
+import {
+  DiGit,
+} from 'react-icons/di';
 
 import {
   Container,
@@ -10,11 +25,16 @@ import {
   Main,
   Sticky,
   Sidebar,
+  SkillWrapper,
+  Skill,
+  CaptionWrapper,
 } from './styles';
 
 import Navbar from '../../components/Navbar';
 import Card from '../../components/Card';
 import Profile from '../../components/Profile';
+
+import funfacts from '../../assets/images/funfacts.png';
 
 export default class About extends Component {
   render() {
@@ -24,7 +44,7 @@ export default class About extends Component {
         <Wrapper>
           <Main>
             <Card
-              icon={<FaGithub />}
+              icon={<FiTerminal />}
               title="introdução"
               content="Desde os 11 anos sou fascinada pela área de tecnologia,
               especialmente programação. Agora, com 18, estou determinada a
@@ -34,34 +54,125 @@ export default class About extends Component {
               construir uma carreira como desenvolvedora de software."
             />
             <Card
-              icon={<FaGithub />}
+              icon={<FiAtSign />}
               title="skills"
-              content="Desde os 11 anos sou fascinada pela área de tecnologia,
-              especialmente programação. Agora, com 18, estou determinada a
-              construir uma carreira como desenvolvedora de software.
-              Desde os 11 anos sou fascinada pela área de tecnologia,
-              especialmente programação. Agora, com 18, estou determinada a
-              construir uma carreira como desenvolvedora de software."
+              content={
+                (
+                  <>
+                    <CaptionWrapper>
+                      <h3>principais</h3>
+                      <SkillWrapper>
+                        <Skill>
+                          <IconContext.Provider value={{ size: '64px', color: '#f0db4f' }}>
+                            <FaJs />
+                          </IconContext.Provider>
+                          <span>Javascript</span>
+                        </Skill>
+                        <Skill>
+                          <IconContext.Provider value={{ size: '64px', color: '#61dbfb' }}>
+                            <FaReact />
+                          </IconContext.Provider>
+                          <span>React</span>
+                        </Skill>
+                        <Skill>
+                          <IconContext.Provider value={{ size: '64px', color: '#3c873a' }}>
+                            <FaNodeJs />
+                          </IconContext.Provider>
+                          <span>Node.js</span>
+                        </Skill>
+                        <Skill>
+                          <IconContext.Provider value={{ size: '64px', color: '#f1502f' }}>
+                            <DiGit />
+                          </IconContext.Provider>
+                          <span>Git</span>
+                        </Skill>
+                      </SkillWrapper>
+                    </CaptionWrapper>
+                    <CaptionWrapper>
+                      <h3>outros</h3>
+                      <p>
+                        #html5 #css3 #bootstrap #restAPIs #axios #express.js
+                        #redux #redux-saga #postgres #mongodb #docker #eslint
+                        #editorconfig #styled-components #prop-types #sass
+                      </p>
+                    </CaptionWrapper>
+                  </>
+                )
+              }
             />
             <Card
-              icon={<FaGithub />}
+              icon={<FiCoffee />}
               title="interesses"
-              content="Desde os 11 anos sou fascinada pela área de tecnologia,
-              especialmente programação. Agora, com 18, estou determinada a
-              construir uma carreira como desenvolvedora de software.
-              Desde os 11 anos sou fascinada pela área de tecnologia,
-              especialmente programação. Agora, com 18, estou determinada a
-              construir uma carreira como desenvolvedora de software."
+              content={
+                (
+                  <p>
+                    Sou apaixonada pelo conhecimento e a programação é
+                    um meio de por minhas ideias em prática, além de exercitar a minha
+                    criatividade. Também gosto de passar o tempo com a minha
+                    família (incluindo meus 3 gatos haha) e adoro tomar um café
+                    - sem açúcar, por favor! :P Um dos meus hobbies favoritos é o
+                    aprendizado de línguas e até agora já tenho confiança no
+                    Inglês e Francês - dá uma olhada no meu
+                    <a
+                      href="https://www.duome.eu/lul_ly"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    > Duolingo
+                    </a>!
+                  </p>
+                )
+              }
             />
             <Card
-              icon={<FaGithub />}
-              title="interesses"
-              content="Desde os 11 anos sou fascinada pela área de tecnologia,
-              especialmente programação. Agora, com 18, estou determinada a
-              construir uma carreira como desenvolvedora de software.
-              Desde os 11 anos sou fascinada pela área de tecnologia,
-              especialmente programação. Agora, com 18, estou determinada a
-              construir uma carreira como desenvolvedora de software."
+              icon={<FiStar />}
+              title="estudos"
+              content={
+                (
+                  <>
+                    <CaptionWrapper>
+                      <h3>acadêmico</h3>
+                      <p>
+                        Técnico, Informática | Instituto Federal de Sergipe - IFS
+                        <span>junho/2015 - agosto/2018</span>
+                      </p>
+                    </CaptionWrapper>
+                    <CaptionWrapper>
+                      <h3>cursos</h3>
+                      <p>
+                        <a href="https://rocketseat.com.br/bootcamp" target="_blank" rel="noopener noreferrer">
+                          Bootcamp GoStack 6.0 | Rocketseat
+                        </a>
+                        <a href="https://www.udemy.com/course/ultimate-web/" target="_blank" rel="noopener noreferrer">
+                          Beginner Full Stack Web Development: HTML, CSS, React & Node | Devslopes
+                        </a>
+                        <a href="https://www.udemy.com/course/curso-kotlin-android/" target="_blank" rel="noopener noreferrer">
+                          Curso Completo de Kotlin para Desenvolvimento Android | AndroidPro Blog
+                        </a>
+                        <a href="https://www.udemy.com/course/git-e-github-para-iniciantes/" target="_blank" rel="noopener noreferrer">
+                          Git e Github para Iniciantes | Willian Justen
+                        </a>
+                        <a href="https://www.udemy.com/course/git-e-github-na-vida-real" target="_blank" rel="noopener noreferrer">
+                          Git e Github na Vida Real | Willian Justen
+                        </a>
+                        <a href="https://www.udemy.com/course/curso-completo-do-desenvolvedor-android/" target="_blank" rel="noopener noreferrer">
+                          Curso Completo de Desenvolvimento Android | Jamilton Damasceno
+                        </a>
+                      </p>
+                    </CaptionWrapper>
+                  </>
+                )
+              }
+            />
+            <Card
+              icon={<FaCode />}
+              title="diversos"
+              content={
+                (
+                  <a href="https://sourcerer.io/luisabfs" target="_blank" rel="noopener noreferrer">
+                    <img src={funfacts} alt="" />
+                  </a>
+                )
+              }
             />
           </Main>
           <Sidebar>
--- src/pages/sobre/styles.js
+++ src/pages/sobre/styles.js
@@ -30,3 +30,66 @@ export const Sticky = styled.div`
   position: sticky;
   top: 20px;
 `;
+
+export const FunFacts = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #f2f2f2;
+  margin: 20px 0;
+  padding: 20px;
+`;
+
+export const SkillWrapper = styled.div`
+  display: flex;
+  margin: 0 auto;
+`;
+
+export const Skill = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  margin: 0 40px;
+
+  svg {
+    margin: 5px 0 5px 0;
+  }
+`;
+
+export const CaptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h3 {
+    width: 100px;
+    font-size: 18px;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+    padding: 7px;
+    border-bottom: 2px solid #656565;
+    margin-bottom: 20px;
+  }
+
+  p {
+    display: flex;
+    flex-direction: column;
+
+    a {
+      color: #88c6dd;
+      font-weight: 700;
+      text-decoration: none;
+      margin-bottom: 5px;
+
+      &:hover {
+        color: #6594a5;
+      }
+    }
+
+    span {
+      font-size: 15px;
+      letter-spacing: 1.11px;
+      margin-top: 5px;
+    }
+  }
+`;
