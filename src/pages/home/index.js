dc85ac46827f8bab6b24428e964ff9b9af11dd9d
Luísa Barros <qlullyq@gmail.com>
2019-10-24 21:41:51 -0300

    feat: increase photos number

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
-import { Bottom, Wrapper, ContactWrap, InstaWrap } from './styles';
+import {
+  Bottom,
+  Wrapper,
+  ContactWrap,
+  InstaWrap,
+} from './styles';
 
 import Header from '../../components/Header';
 import About from '../../components/About';
@@ -16,8 +21,8 @@ export default class Home extends Component {
   }
 
   async componentDidMount() {
-    let token = '13066341700.0243229.dc58665d3bde4286b7dd1adf8977e0e0';
-    let count = 6;
+    const token = '13066341700.0243229.dc58665d3bde4286b7dd1adf8977e0e0';
+    const count = 9;
 
     const { data: allImages } = await api.get(`v1/users/self/media/recent/?access_token=${token}&count=${count}`);
     this.setState({ images: allImages.data });
