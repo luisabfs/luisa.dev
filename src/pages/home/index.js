4b410b521aab58a500e95b82fcdef4f5da299da7
Luísa Barros <qlullyq@gmail.com>
2020-12-11 15:59:26 -0300

    refactor: separate instagram provider from home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import api from '../../services/api';
+import React from 'react';
 
 import {
   Bottom, Wrapper, ContactWrap, InstaWrap,
@@ -12,41 +11,23 @@ import Contact from '../../components/Contact';
 import Instagram from '../../components/Instagram';
 import Footer from '../../components/Footer';
 
-export default class Home extends Component {
-  state = {
-    images: [],
-  };
+const Home = () => (
+  <>
+    <Header />
+    <About />
+    <Projects />
+    <Bottom>
+      <Wrapper>
+        <ContactWrap id="contact">
+          <Contact />
+        </ContactWrap>
+        <InstaWrap>
+          <Instagram />
+        </InstaWrap>
+      </Wrapper>
+    </Bottom>
+    <Footer />
+  </>
+);
 
-  async componentDidMount() {
-    const token = '13066341700.0243229.dc58665d3bde4286b7dd1adf8977e0e0';
-    const count = 6;
-
-    const { data: allImages } = await api.get(
-      `v1/users/self/media/recent/?access_token=${token}&count=${count}`,
-    );
-    this.setState({ images: allImages.data });
-  }
-
-  render() {
-    const { images } = this.state;
-
-    return (
-      <>
-        <Header />
-        <About />
-        <Projects />
-        <Bottom>
-          <Wrapper>
-            <ContactWrap id="contact">
-              <Contact />
-            </ContactWrap>
-            <InstaWrap>
-              <Instagram images={images} />
-            </InstaWrap>
-          </Wrapper>
-        </Bottom>
-        <Footer />
-      </>
-    );
-  }
-}
+export default Home;
