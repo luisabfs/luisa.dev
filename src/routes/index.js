705138faa2e82e106124d2792ac9547c484645c7
Luísa Barros <qlullyq@gmail.com>
2020-12-22 19:15:38 -0300

    refactor: replace hashrouter with browserrouter and add public url as basename

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/home';
 import About from '../pages/sobre';
@@ -8,14 +8,14 @@ import Projects from '../pages/projetos';
 import Links from '../pages/links';
 
 const Routes = () => (
-  <HashRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/sobre" component={About} />
       <Route path="/projetos" component={Projects} />
       <Route path="/links" component={Links} />
     </Switch>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default Routes;
