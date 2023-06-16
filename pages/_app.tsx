c215db66b85375721a6f157551a37ee4fbe47c9c
Luisa Barros <luisa.barros.dev@gmail.com>
2023-06-16 15:57:19 -0300

    fix: image preview path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <title>Luísa Barros - Mobile Dev</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
-          <meta key="image" property="og:image" content="https://raw.githubusercontent.com/luisabfs/luisabfs.github.io/source/src/assets/images/print.png" />
+          <meta key="image" property="og:image" content="https://raw.githubusercontent.com/luisabfs/luisabfs.github.io/source/public/assets/images/print.png" />
           <meta key="title" property="og:title" content="Luísa Barros - Desenvolvedora Mobile" />
           <meta key="description" property="og:description" content="React Native, React, Typescript" />
         </Head>
